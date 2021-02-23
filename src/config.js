5b58467c65fa8ff95a1f03d9c50ace78273b19f2
Andrew <ramettaandrewj@gmail.com>
2021-02-23 15:41:43 -0500

    initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+  API_ENDPOINT: 'https://safe-oasis-81782.herokuapp.com',
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  API_KEY: process.env.REACT_APP_API_KEY,
+}
---
+++ src/notes-helpers.js
@@ -0,0 +1,17 @@
+export const findFolder = (folders = [], folderId) =>
+  folders.find((folder) => {
+    return folder.id === folderId;
+  });
+
+export const findNote = (notes = [], noteId) =>
+  notes.find((note) => note.id + "" === noteId);
+
+export const getNotesForFolder = (notes = [], folderId) =>
+  !folderId
+    ? notes
+    : notes.filter((note) => {
+        return note.folder_id + "" === folderId;
+      });
+
+export const countNotesForFolder = (notes = [], folderId) =>
+  notes.filter((note) => note.folder_id === folderId).length;
