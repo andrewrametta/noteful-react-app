f23ceef3d369a360f50236c72f014916a60193f0
Andrew <ramettaandrewj@gmail.com>
2021-02-23 16:24:52 -0500

    added config and updated snapshot

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://safe-oasis-81782.herokuapp.com',
+  API_ENDPOINT: "https://localhost:8000/api",
   //API_ENDPOINT: 'http://localhost:8000/api',
-  API_KEY: process.env.REACT_APP_API_KEY,
-}
+};
